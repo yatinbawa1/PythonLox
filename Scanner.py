9b03871e724d8cd7a8729232a3b43c62ed3620af
yatinbawa1 <yatinabc3@gmail.com>
2022-04-13 00:47:53 +0530

    Scanner Complete

--- Scanner.py
+++ Scanner.py
@@ -1,4 +1,5 @@
 from ast import Continue
+from pickle import NONE
 from Token import Token
 from TokenType import TokenType
 
@@ -6,12 +7,31 @@ from TokenType import TokenType
 class Scanner:
     start = 0
     current = 0
-    line = 0
+    line = 1
     tokens = []
     source = ""
 
-    def __init__(self):
-        self.source = '"Name"'
+    keywords = {
+        "and": TokenType.AND,
+        "class": TokenType.CLASS,
+        "else": TokenType.ELSE,
+        "false": TokenType.FALSE,
+        "fun": TokenType.FUN,
+        "for": TokenType.FOR,
+        "if": TokenType.IF,
+        "null": TokenType.NULL,
+        "or": TokenType.OR,
+        "print": TokenType.PRINT,
+        "return": TokenType.RETURN,
+        "super": TokenType.SUPER,
+        "this": TokenType.THIS,
+        "true": TokenType.TRUE,
+        "var": TokenType.VAR,
+        "while": TokenType.WHILE,
+    }
+
+    def __init__(self, source):
+        self.source = source
 
     def isAtEnd(self):
         return self.current >= len(self.source)
@@ -20,9 +40,9 @@ class Scanner:
         while(not self.isAtEnd()):
             self.start = self.current
             self.scanToken()
-        self.addToken(TokenType.EOF, None)
-        print("{} is of type{}".format(
-            self.tokens[0].literal, self.tokens[0].type))
+
+        self.tokens.append(Token(TokenType.EOF, None, None, self.line+1))
+        return self.tokens
 
     def advance(self):
         c = self.source[self.current]
@@ -42,49 +62,122 @@ class Scanner:
             return '/0'
         return self.source[self.current]
 
+    def isDigit(self, c):
+        if (c >= '0' and c <= '9'):
+            return True
+        else:
+            return False
+
+    def isAplha(self, c):
+        val = (c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z') or c == '_'
+        return val
+
     def scanToken(self):
         c = self.advance()
+
         if c == '(':
             self.addToken(TokenType.LEFT_PAREN, None)
+
         elif c == ")":
             self.addToken(TokenType.RIGHT_PAREN, None)
+
         elif c == "{":
             self.addToken(TokenType.LEFT_BRACE, None)
+
         elif c == "}":
             self.addToken(TokenType.RIGHT_BRACE, None)
+
         elif c == ",":
             self.addToken(TokenType.COMMA, None)
+
         elif c == ".":
             self.addToken(TokenType.DOT, None)
+
         elif c == "+":
             self.addToken(TokenType.PLUS, None)
+
         elif c == "-":
             self.addToken(TokenType.MINUS, None)
+
         elif c == ";":
             self.addToken(TokenType.SEMICOLON, None)
+
         elif c == "*":
             self.addToken(TokenType.STAR, None)
+
         elif c == "/":
-            # self.addToken(TokenType.TokenType.RIGHT_BRACE,None)
             if(self.match('/')):
+                print(self.peek() != '\n')
+                print(self.isAtEnd())
                 while (self.peek() != '\n' and self.isAtEnd()):
                     self.advance()
             else:
                 self.addToken(TokenType.SLASH, None)
+
+        elif c == "<":
+            if(self.match("=")):
+                self.addToken(TokenType.GREATER_EQUAL, None)
+            else:
+                self.addToken(TokenType.GREATER, None)
+
+        elif c == ">":
+            if self.match("="):
+                self.addToken(TokenType.LESS_EQUAL, None)
+            else:
+                self.addToken(TokenType.LESS, None)
+
         elif c == (' ' or '\t' or '\r'):
             # Do nothing
             Continue
         elif c == '\n':
-            line = line + 1
+            self.line += 1
         elif c == '"':
             self.string()
+        else:
+            if(self.isDigit(c)):
+                self.number()
+            elif(self.isAplha(c)):
+                self.identifier()
+            else:
+                print("Error {}".format(self.source[0:self.current]))
+
+    def peekNext(self):
+        if (self.isAtEnd()):
+            return '/0'
+        return self.source[self.current+1]
+
+    def number(self):
+        while(self.isDigit(self.peek())):
+            self.advance()
+        if self.peek() == '.' and self.isDigit(self.peekNext()):
+            self.advance()
+            while(self.isDigit(self.peek())):
+                self.advance()
+        self.addToken(TokenType.NUMBER, float(
+            self.source[self.start:self.current]))
+
+    def isAphaNumeric(self, c):
+        return self.isDigit(c) or self.isAplha(c)
+
+    def identifier(self):
+        while(self.isAphaNumeric(self.peek())):
+            self.advance()
+
+        text = self.source[self.start:self.current]
+        type = self.keywords.get(text)
+
+        if type == None:
+            type = TokenType.IDENTIFIER
+
+        self.addToken(type, None)
 
     def string(self):
         while(self.peek() != '"' and (not self.isAtEnd())):
             if self.peek() == '\n':
-                line = line + 1
+                self.line += 1
             self.advance()
         value = self.source[self.start + 1: self.current]
+        self.advance()
         self.addToken(TokenType.STRING, value)
 
     def addToken(self, type, literal):
@@ -92,5 +185,17 @@ class Scanner:
         self.tokens.append(Token(type, text, literal, self.line))
 
 
-a = Scanner()
-a.scanTokens()
+f = open("./file.lox", 'r')
+source = str(f.read())
+
+scan = Scanner(source)
+tokens = scan.scanTokens()
+l = 1
+for x in tokens[0:-1]:
+    if x.line > l:
+        l = l + 1
+        print("\n")
+    print(x.lexeme, end=" ")
+
+# for x in tokens:
+#     print("{} {}".format(x.lexeme, x.line))
--- script.py
+++ script.py
@@ -1,12 +1,27 @@
-sc = "LEFT_BRACE,LEFT_PAREN,RIGHT_PAREN,RIGHT_BRACE,COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,  BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL,    LESS, LESS_EQUAL,    IDENTIFIER, STRING, NUMBER, AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NULL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE, EOF"
+# sc = "LEFT_BRACE,LEFT_PAREN,RIGHT_PAREN,RIGHT_BRACE,COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,  BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL,    LESS, LESS_EQUAL,    IDENTIFIER, STRING, NUMBER, AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NULL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE, EOF"
+import string
+from tokenize import String
 
-f = open("TokenType.py", 'w')
-f.write("import enum\n")
-f.write('\n \n')
-f.write("@enum.unique\n")
-f.write("class TokenType(enum.Enum):\n")
-novalue = sc.replace(' ', "")
-values = novalue.split(",")
-f.write("\n")
-for x in values:
-    f.write('    {} = "{}"\n'.format(x, x))
+
+# key = "AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,"
+# f = open("keywords.py", 'w')
+
+# f.write("import enum\n")
+# f.write('\n \n')
+# f.write("@enum.unique\n")
+# f.write("class TokenType(enum.Enum):\n")
+# novalue = sc.replace(' ', "")
+# values = novalue.split(",")
+# f.write("\n")
+# for x in values:
+#     f.write('    {} = "{}"\n'.format(x, x))
+
+# f.write("keywords = {\n")
+# novalue = key.replace(' ', "")
+# values = novalue.split(",")
+
+# print(values)
+# for x in values:
+#     f.write('"{}":TokenType.{}, \n'.format(x.lower(), x))
+
+# f.write("\n}")
